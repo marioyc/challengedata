72321537b785a4d6d35dbc1a9204b4ef98b490eb
syltruong <syltruong@gmail.com>
2017-01-31 17:04:43 +0100

    removed data processing methods

--- lib/method1.py
+++ lib/method1.py
@@ -5,42 +5,6 @@ import pickle
 from sklearn.linear_model import LogisticRegression
 from public_score_auc import score_function
 
-
-embedding_data = pickle.load(open('data/polyglot-fr.pkl', 'rb'))
-word_embedding = {}
-
-for i in range(len(embedding_data[0])):
-    word_embedding[ embedding_data[0][i] ] = embedding_data[1][i]
-
-
-def process_input(X):
-    n = len(X)
-    ret = numpy.zeros((n,65))
-
-    stop = set(nltk.corpus.stopwords.words('french'))
-
-    for i in range(n):
-        tokens = nltk.word_tokenize(X[i]['content'], language='french')
-        tokens = [w.lower() for w in tokens]
-        tokens = [w for w in tokens if not w in stop]
-
-        embedding = numpy.zeros(64)
-        cont = 0
-        for token in tokens:
-            if token in word_embedding:
-                embedding += word_embedding[token]
-                cont += 1
-        if cont == 0:
-            #print tokens
-            pass
-        else:
-            embedding /= cont
-
-        ret[i, 0:64] = embedding
-        ret[i, 64] = X[i]['stars']
-
-    return ret
-
 def predict(Xtrain,Ytrain,Xtest,X_percentage,output_path):
 
 	split_idx = int((float(X_percentage) / float(100)) * len(Xtrain))
