2aeed7d5fa8073723740df07fe9589c27447dfc3
Mario Ynocente Castro <ycmario@gmail.com>
2017-01-31 09:16:21 +0100

    separate entries

---
+++ load_data.py
@@ -0,0 +1,57 @@
+import csv
+
+def load_input_file(filename, ninput, firstID):
+    f = open(filename, 'r')
+    lines = f.readlines()
+    aux = ""
+    X = []
+
+    for line in lines:
+        aux += line
+
+    pos = 0
+    starts = []
+    for i in range(ninput):
+        target = "\n" + str(firstID + i) + ";"
+        L = len(target)
+
+        pos = aux.index(target, pos)
+        starts.append(pos + 1)
+        pos += L
+    starts.append(len(aux))
+
+    for i in range(ninput):
+        X.append(aux[starts[i]:starts[i + 1]])
+
+    return X
+
+def load_output_file(filename):
+    f = open(filename, 'rU')
+    reader = csv.reader(f, delimiter=';')
+    Y = []
+
+    reader.next()
+    for row in reader:
+        Y.append(int(row[1]))
+
+    return Y
+
+def load_data():
+    Xtrain = load_input_file('data/input_train.csv', 80000, 1)
+    Xtest = load_input_file('data/input_test.csv', 36395, 80001)
+    Ytrain = load_output_file('data/output_train.csv')
+    assert len(Xtrain) == len(Ytrain)
+    return Xtrain, Xtest, Ytrain
+
+if __name__ == '__main__':
+    Xtrain, Xtest, Ytrain = load_data()
+
+    for i in range(5):
+        print Xtrain[i], Ytrain[i]
+    for i in range(5):
+        print Xtrain[-i - 1], Ytrain[i]
+
+    for i in range(5):
+        print Xtest[i]
+    for i in range(5):
+        print Xtest[-i - 1]
