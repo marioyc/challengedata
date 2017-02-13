4bc3fe6684cc04608b103a2ae8c549f481d307c5
marioyc <ycmario@gmail.com>
2017-02-13 15:14:48 +0100

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+apt-get install -y python-pip python-matplotlib python-setuptools
+
+pip install numpy scipy
+
 pip install nltk
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
+
+pip install word2vec
+
+pip install -U scikit-learn
+
+git clone --recursive https://github.com/dmlc/xgboost
+cd xgboost
+make -j4
+cd python-package
+python setup.py install
