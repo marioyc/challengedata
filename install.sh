a3e6e6f9ed54e9c198f67746dce905e02ebc9635
marioyc <ycmario@gmail.com>
2017-02-13 16:05:02 +0100

    add missing dependency

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@ pip install nltk
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
 
+pip install Cython
 pip install word2vec
 
 pip install -U scikit-learn
 
-git clone --recursive https://github.com/dmlc/xgboost
-cd xgboost
-make -j4
-cd python-package
-python setup.py install
+#git clone --recursive https://github.com/dmlc/xgboost
+#cd xgboost
+#make -j4
+#cd python-package
+#python setup.py install
