fca9ecdcee4c3b2c3a553ba960c8e97c3e638deb
marioyc <ycmario@gmail.com>
2017-02-18 01:45:32 +0100

    fix embedding freeze, add auc to metrics displayed

--- lib_fit/method5.py
+++ lib_fit/method5.py
@@ -12,11 +12,13 @@ class AUCCallback(Callback):
     def __init__(self):
         super(Callback, self).__init__()
 
-    def on_epoch_end(self, epoch, logs={}):
+    def on_train_begin(self, logs=None):
+        self.params['metrics'] += ['val_auc']
+
+    def on_epoch_end(self, epoch, logs=None):
         y_pred = self.model.predict_proba(self.model.validation_data[0], verbose=0)
         auc = roc_auc_score(self.model.validation_data[1], y_pred)
-        print "AUC: {:.6f}".format(auc)
-
+        logs['val_auc'] = auc
 
 def predict(Xtrain, Ytrain, Xtest, embeddings_matrix, X_percentage, output_path):
     split_idx = int((float(X_percentage) / float(100)) * len(Xtrain))
@@ -46,7 +48,7 @@ def predict(Xtrain, Ytrain, Xtest, embeddings_matrix, X_percentage, output_path)
                         weights=[embeddings_matrix],
                         input_length=maxlen,
                         dropout=0.3))
-    model.layers[0].trianable = False
+    model.layers[0].trainable = False
     #model.add(Convolution1D(nb_filter=nb_filter,
     #                        filter_length=filter_length,
     #                        border_mode='valid',
@@ -64,20 +66,26 @@ def predict(Xtrain, Ytrain, Xtest, embeddings_matrix, X_percentage, output_path)
 
     print "-------- starting training of the model"
     batch_size       =  64
-    nb_epoch         =  7
+    nb_epoch         =  50
     auc_callback = AUCCallback()
     history = model.fit(Xtrain, Ytrain, batch_size=batch_size, nb_epoch=nb_epoch, validation_split=0.3, callbacks=[auc_callback])
 
     plt.figure(1)
-    plt.subplot(1,2,1)
+    plt.subplot(1,3,1)
     plt.plot(range(1,nb_epoch + 1), history.history['loss'], 'b', range(1,nb_epoch + 1), history.history['val_loss'], 'r')
     plt.ylabel('loss')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
 
-    plt.subplot(1,2,2)
+    plt.subplot(1,3,2)
     plt.plot(range(1,nb_epoch + 1), history.history['acc'], 'b', range(1,nb_epoch + 1), history.history['val_acc'], 'r')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'], loc='upper left')
+
+    plt.subplot(1,3,3)
+    plt.plot(range(1,nb_epoch + 1), history.history['val_auc'], 'r')
+    plt.ylabel('auc')
+    plt.xlabel('epoch')
+    plt.legend(['validation'], loc='upper left')
     plt.show()
